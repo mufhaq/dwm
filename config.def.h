a074273d856aafd15be959bca1f4752eb8d52e18
mufHaQ <masbrowmess33@gmail.com>
2022-07-05 18:34:45 +0700

    [config] update

--- config.def.h
+++ config.def.h
@@ -7,13 +7,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
-    "FiraCode Nerd Font:style=Medium:pixelsize=13",
+    "JetBrainsMono Nerd Font:style=Medium:pixelsize=13",
     //"NotoMono Nerd Font:style=Book:size=10",
     //"NotoMono Nerd Font Mono:style=Book:size=10",
     //"NotoEmoji Nerd Font Mono:style=Book:size=10",
     //"Noto Color Emoji:style=Regular:size=10",
 };
-static const char dmenufont[]       = "FiraCode Nerd Font:style=Medium:pixelsize=13";
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Medium:pixelsize=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
