cdd57fd17f13ca9b39d7a200e569b68b901de594
mufhaq <masbrowmess33@gmail.com>
2024-10-20 22:21:03 +0700

    change termcmd value to alacritty

--- config.def.h
+++ config.def.h
@@ -75,7 +75,7 @@ static const char *downbright[] = { "/usr/bin/xbacklight", "-dec", "5" };
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
